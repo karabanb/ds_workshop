8fd5c3114b8baeb9d7d42a0bbdd808a21ee0aa67
karabanb <bkarabands@gmail.com>
2019-08-06 15:35:32 +0200

    skewness was added

--- excercises/014_StatystykaOpisowaSkosnosc.R
+++ excercises/014_StatystykaOpisowaSkosnosc.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(moments)
+
 #pozycyjny wspolczynnik asymetrii A = Q3 + Q1 - 2 Me / Q3 - Q1
 
 asymetry_q <- function (x) {
@@ -11,8 +14,14 @@ p2 <- function(x){
 
 load('data/012_skewed_data.Rdata')
 
-skewed_data
+skewed_data %>% group_by(skewness) %>% summarise(asymetry_q(x), p2(x))
+skewed_data %>% group_by(skewness) %>% summarise(asymetry_q(x*100), p2(x))
 
 skew <- function(x){
   sum((x-mean(x))^3)/length(x)/sd(x)^3
 }
+
+skewed_data %>% filter(skewness == 'left skewed') %>% summarise(skew(x), skewness(x))
+
+bench_skewness <- microbenchmark(skewness(skewed_data$x), skew(skewed_data$x), 1000)
+
